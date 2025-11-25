5329c6cb0a5ca5303016d407a2111f3da620462e
Miguel21S <leaismiguelss@gamail.com>
2025-11-26 00:47:04 +0100

    feat: creación del store.js

---
+++ src/app/store.js
@@ -0,0 +1,29 @@
+import { persisteReducer, persistStore } from "redux-persist"
+import { combineReducers } from "redux";
+import { userSlice } from "./slices/userSlice";
+import { profileReducer } from "./slices/profileSlice";
+import storage from "redux-persist/lib/storage"
+import { configureStore } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+    user: userSlice,
+    profile: profileReducer
+
+})
+
+const persistcnfig = {
+    key: 'root',
+    storage
+}
+
+const persistedReducer = persisteReducer(persistcnfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
+})
+
+export const persitor = persistStore(store);
+export default store;
