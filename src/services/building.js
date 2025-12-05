86f0119899a2d7d040f9fbf229a7ebb3b2c47cd8
Miguel21S <leaismiguelss@gamail.com>
2025-12-05 22:09:20 +0100

    Feat: creación de la ruta para eliminar edificio

--- src/services/building.js
+++ src/services/building.js
@@ -1,10 +1,9 @@
-const root = 'http://localhost:2025/api/'
-
+const root = 'http://localhost:2025/api/buildings/'
 
 ////////////////   ROOT GET ALL BUILDING
 export const rootAllBuildings = async (token) => {
     try {
-        const response = await fetch(`${root}buildings/auth/admin/list`, {
+        const response = await fetch(`${root}auth/admin/list`, {
             method: 'GET',
             headers: {
                 "Content-Type": "application/json",
@@ -25,6 +24,35 @@ export const rootAllBuildings = async (token) => {
         }
         console.log('DESPUES DEL DATA:', data)
         return data;
+    } catch (error) {
+        console.error("Error fetching buildings:", error.message);
+        return { success: false, message: error.message };
+    }
+}
+
+////////////////   ROOT DELETE BUILDING BY ID
+export const rootDeleteBuilding = async(token, id)=>{
+    const options = {
+        method: 'DELETE',
+        headers:{
+            'content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    }
+
+    try {
+        const response = await fetch(`${root}auth/admin/delete/${id}`, options)
+        if(!response.ok){
+            const errorData = await response.json()
+            throw new Error(errorData.message || 'Error en la eliminar edificio')
+        }
+
+        const data = await response.json()
+        if(!data.success){
+             throw new Error(data.message);
+        }
+
+        return data
     } catch (error) {
         console.error("Error fetching buildings:", error.message);
         return { success: false, message: error.message };
